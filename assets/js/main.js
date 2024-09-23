f69c54d277493d4c1d13cf3bcaa0b492882fb598
gopalubt <ranagopal29@gmail.com>
2024-09-23 18:45:42 +0530

    api call correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,38 @@
-
 const formEl = document.forms.namedItem("enquiry");
 const base_API_Url = "https://gp-cloud.vercel.app";
-let message = ""
-formEl.addEventListener("submit",  (event)=>{
-    event.preventDefault();
-    let formData={
-      "name" :  formEl.name.value.trim(), 
-      "email":  formEl.email.value.trim(), 
-      "mobile":  formEl.mobile.value.trim(), 
-      "message": formEl.message.value.trim() 
+let message = "";
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  let formData = {
+    "name": formEl.name.value.trim(),
+    "email": formEl.email.value.trim(),
+    "mobile": formEl.mobile.value.trim(),
+    "message": formEl.message.value.trim(),
+  };
+
+  fetch(`${base_API_Url}/api/v1/enquiry`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", 
+      "apihost": "https://gopalubt.github.io/rana-portfolio"
+    },
+    body: JSON.stringify(formData) 
+  })
+  .then(res => {
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
     }
-    debugger
-   fetch(`${base_API_Url}/api/v1/enquiry`, {
-      method: "POST",
-      headers: {apihost: 'https://gopalubt.github.io/rana-portfolio'},
-      body: formData
-    })
-    .then(res=>JSON.parse(res))
-    .then(result => {
-      debugger
-      console.log(result);
-      formEl.reset();
-    })
-    .catch((error)=>console.log(error))
-})
+    return res.json(); 
+  })
+  .then(result => {
+    console.log(result);
+    message = "Send Successfully!"
+    formEl.reset();  
+  })
+  .catch((error) => {
+    message = "There was an error with the fetch operation!"
+    console.error('There was an error with the fetch operation:', error);
+  });
+});
